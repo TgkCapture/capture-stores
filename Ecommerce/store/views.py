82dd14565b60f38e66225155cf4d2425936b0634
tgkcapture <71700867+TgkCapture@users.noreply.github.com>
2022-03-09 12:28:34 +0200

    update added

--- Ecommerce/store/views.py
+++ Ecommerce/store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+from django.http import JsonResponse
 
 def store(request):
     products = Product.objects.all()
@@ -30,3 +31,6 @@ def checkout(request):
 
     context = {'items':items, 'order':order}
     return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+    return JsonResponse('Item was added', safe=False)
