57269cdefd620cfc797353d1bd96d0a9af22e781
tgkcapture <71700867+TgkCapture@users.noreply.github.com>
2022-03-18 17:00:34 +0200

    update added

--- Ecommerce/store/views.py
+++ Ecommerce/store/views.py
@@ -1,10 +1,11 @@
+from itertools import product
 from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
 import json
 import datetime
 
-from . utils import cookieCart, cartData
+from . utils import cookieCart, cartData, guestOrder
 
 def store(request):
 
@@ -95,6 +96,7 @@ def cart(request):
     context = {'items':items, 'order':order, 'cartItems':cartItems}
     return render(request, 'store/cart.html', context)
 
+
 def checkout(request):
     # code moved to utils.py
 
@@ -147,6 +149,12 @@ def updateItem(request):
     return JsonResponse('Item was added', safe=False)
 
 
+
+# make changes here
+# from django.views.decorators.csrf import csrf_exempt
+
+# @csrf_exempt
+
 def processOrder(request):
     transaction_id = datetime.datetime.now().timestamp()
     data = json.loads(request.body)
@@ -154,6 +162,55 @@ def processOrder(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        # total = float(data['form']['total'])
+        # order.transaction_id = transaction_id
+
+        # if total == float(order.get_cart_total):
+        #     order.complete = True
+        # order.save()
+
+        # if order.shipping == True:
+        #     ShippingAddress.objects.create(
+        #     customer=customer,
+        #     order=order,
+        #     address=data['shipping']['address'],
+        #     city=data['shipping']['city'],
+        #     state=data['shipping']['state'],
+        #     zipcode=data['shipping']['zipcode'],
+        # )
+    else:
+        customer, order = guestOrder(request, data)
+
+        # print('User is not logged in..')
+
+        # print('COOKIES:', request.COOKIES)
+        # name = data['form']['name']
+        # email = data['form']['email']
+
+        # cookieData = cookieCart(request)
+        # items = cookieData['items']
+
+        # customer, created = customer.objects.get_or_create(
+        #     email=email,
+        # )
+
+        # customer.name = name
+        # customer.save()
+
+        # order = Order.objects.create(
+        #     customer=customer,
+        #     complete=False,
+        # )
+
+        # for item in items:
+        #     product = Product.objects.get(id=item['product']['id'])
+
+        #     orderItem = OrderItem.objects.create(
+        #     product=product,
+        #     order=order,
+        #     quantity=item['quantity']
+        #     )
+
         total = float(data['form']['total'])
         order.transaction_id = transaction_id
 
@@ -163,13 +220,13 @@ def processOrder(request):
 
         if order.shipping == True:
             ShippingAddress.objects.create(
-                customer=customer,
-                order=order,
-                address=data['shipping']['address'],
-                city=data['shipping']['city'],
-                state=data['shipping']['state'],
-                zipcode=data['shipping']['zipcode'],
-            )
-    else:
-        print('User is not logged in..')
+            customer=customer,
+            order=order,
+            address=data['shipping']['address'],
+            city=data['shipping']['city'],
+            state=data['shipping']['state'],
+            zipcode=data['shipping']['zipcode'],
+        )
+
     return JsonResponse('Payment Complete', safe=False)
+ 
